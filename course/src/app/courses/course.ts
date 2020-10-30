6c010fbc2cccb625117f952d8f33d6b51933fba4
RafaelaSMiranda <rafaela.silva.m@outlook.com>
2020-10-30 00:38:17 -0300

    Primeira parte do estudo de Angular

---
+++ course/src/app/courses/course.ts
@@ -0,0 +1,13 @@
+export class Course {
+
+
+  id: number;
+  name: string;
+  imageUrl: string;
+  price: string;
+  code: string;
+  duration: number;
+  rating: number;
+  releaseDate: string;
+
+}
---
+++ course/src/app/star/star.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnChanges } from '@angular/core';
+
+
+@Component({
+
+  selector: 'app-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css'],
+
+})
+
+export class StarComponent implements OnChanges {
+
+  @Input()
+  // variável que receba valor de componente externo
+
+  rating: number = 0;
+  starWidth: number;
+
+
+  ngOnChanges(): void {
+
+    this.starWidth = this.rating * 74/5;
+
+
+
+  }
+
+}
