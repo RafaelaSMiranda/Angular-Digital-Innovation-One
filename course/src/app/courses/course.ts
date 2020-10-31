2f0e484237e32a810ded27808618def9ebb8253d
RafaelaSMiranda <rafaela.silva.m@outlook.com>
2020-10-31 00:26:35 -0300

    Parte 2 - rotas e pipe

--- course/src/app/courses/course.ts
+++ course/src/app/courses/course.ts
@@ -4,10 +4,11 @@ export class Course {
   id: number;
   name: string;
   imageUrl: string;
-  price: string;
+  price: number;
   code: string;
   duration: number;
   rating: number;
   releaseDate: string;
+  description: string;
 
 }
